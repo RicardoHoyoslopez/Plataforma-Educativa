359fed9416c46a65733d01cebc8a0564b48bc92e
RicardoHoyosLopez <Ricardohoyos@unicomfacauca.edu.co>
2025-04-09 20:29:55 -0500

    Listo el registro de los usuarios

--- includes/Conexion.php
+++ includes/Conexion.php
@@ -3,7 +3,7 @@
 $host="localhost";
 $user="root";
 $pass="";
-$bd="plataformaeducativa";
+$bd="prueba";
 
 $conexion=mysqli_connect($host, $user, $pass, $bd);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
